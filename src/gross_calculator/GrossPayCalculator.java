1b608b0993d97c43830c4f73776e24e13e9fccdc
Ngomeli <joshuangomeli@gmail.com>
2026-03-23 18:51:09 +0300

    used scanner to get input from user

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -1,12 +1,21 @@
 package gross_calculator;
-
+import java.util.Scanner;
 public class GrossPayCalculator {
     public static void main(String[] args) {
         //Get the number of hours worked
-        int hours = 40;
+        int hours = 0;
+        System.out.println("Please enter the hours you would like to pay:");
+
+        Scanner input = new Scanner(System.in);
+        hours = input.nextInt();
 
         //Get the hourly pay rate
-        double payRate = 25.50;
+        double payRate = 0;
+        System.out.println("Please enter the amount you would like to pay:");
+
+        payRate = input.nextDouble();
+
+        input.close();
 
         //Multiply hours and pay rate
         double grossPay = payRate * hours;
