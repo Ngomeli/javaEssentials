8c31afe918093b82ef6015bfe3cfdbbe1277ad7c
Ngomeli <joshuangomeli@gmail.com>
2026-04-03 13:23:04 +0300

    working with objects and constructors

---
+++ src/objects/Rectangle.java
@@ -0,0 +1,36 @@
+package objects;
+
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(){
+        length = 0;
+        width = 0;
+    }
+
+    public Rectangle(double length, double width){
+        setLength(length);
+        setWidth(width);
+    }
+
+    public double calculatePerimeter() {
+        return (2 * length) + (2 * width);
+    }
+    public double calculateArea() {
+        return (length * width);
+    }
+
+    public double getLength() {
+        return length;
+    }
+    void setLength(double length) {
+        this.length = length;
+    }
+    double getWidth() {
+        return width;
+    }
+    void setWidth(double width) {
+        this.width = width;
+    }
+}
