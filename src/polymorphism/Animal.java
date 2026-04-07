d58af05e05bfc99a6e7f4f4a96c42a181e0d0deb
Ngomeli <joshuangomeli@gmail.com>
2026-04-08 02:11:05 +0300

    performing polymorphism and object casting

---
+++ src/polymorphism/Animal.java
@@ -0,0 +1,8 @@
+package polymorphism;
+
+public class Animal {
+
+    public void makeSound() {
+        System.out.println("I'm a makeSound");
+    }
+}
---
+++ src/polymorphism/Zoo.java
@@ -0,0 +1,18 @@
+package polymorphism;
+
+public class Zoo {
+
+    public static void main(String[] args) {
+        Dog rocky = new Dog();
+        rocky.fetch();
+        rocky.makeSound();
+
+        Animal sasha = new Dog();
+        sasha.makeSound();
+
+        sasha = new Cat();
+        sasha.makeSound();
+
+        ((Cat)sasha).scratch();
+    }
+}
