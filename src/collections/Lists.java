1cd399342e41a2fd4ba12454b9d906b522a00dea
Ngomeli <joshuangomeli@gmail.com>
2026-04-28 03:30:13 +0300

    working with lists

---
+++ src/collections/Lists.java
@@ -0,0 +1,16 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lists {
+    public static void main(String[] args) {
+        List fruits = new ArrayList();
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("cherry");
+        fruits.add("watermelon");
+
+        System.out.println("Fruits: "+fruits);
+    }
+}
